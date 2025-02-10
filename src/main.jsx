83ebbf1a4c4ab128b708197671a67ab7b2bee184
Yashankkk <126323177+Yashankkk@users.noreply.github.com>
2025-02-10 12:25:11 +0530

    m

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router";
+// import './index.css'
+import App from './App.jsx'
+import Login from './Pages/Login.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+  <Routes>
+  <Route path="/" element={<App />} />
+  <Route path="login" element={<Login/>} />
+  </Routes>
+</BrowserRouter>
+
+)
