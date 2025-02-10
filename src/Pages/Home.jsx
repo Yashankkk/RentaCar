cb9cae4963871a9935fd43c724e90cacd4fc72a3
Yashankkk <126323177+Yashankkk@users.noreply.github.com>
2025-02-10 16:06:46 +0530

    initial commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Home.css"
+
+const Home = () => {
+  return (
+    <div className='div'>
+      <header class="header">
+            <a href="#" class="logo">RideEase</a>
+            <nav class="navbar">
+                <a href="#">Home</a>
+                <a href="blog.html">Blogs</a>
+                <a href="contact.html">Contact</a>
+                <a href="about.html">About Us</a>
+            </nav>
+        </header>
+    </div>
+  )
+}
+
+export default Home
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from "react-router";
 // import './index.css'
 import App from './App.jsx'
 import Login from './Pages/Login.jsx';
+import Home from './Pages/Home.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <Routes>
   <Route path="/" element={<App />} />
   <Route path="login" element={<Login/>} />
+  <Route path="home" element={<Home/>} />  
   </Routes>
 </BrowserRouter>
 
