013d4080d33912679026624216d2798e7b75ec24
Yashankkk <yashankkalsi030@gmail.com>
2025-03-18 17:53:00 +0530

    initial commit

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button, Form, Input, Row, Col } from 'antd';
 import { Typography } from 'antd'
 // import "./Home.css"
 
@@ -13,14 +14,89 @@ const Contact = () => {
             Drop Us a Line
           </Typography.Title>
             <p className='text-gray-700'>Feel free to connect with us through our online channels for updates, news and more.</p>
-            <form action="#" method="post">
-            <label for="Full Name">Full Name</label>
-            <input type="text" name="Full Name" placeholder="Name" ></input>
-            <label for="Email">Email Address</label>
-            <input type="email" name="Email" placeholder="Email"></input><br/><br/>
-            <label for="phone">Phone Number</label>
-            <input type="tel" name="phone" placeholder="Number"></input>
-            </form>
+            <Form className='!mt-10'
+            name="basic"
+            labelCol={{
+              span: 8,
+            }}
+            wrapperCol={{
+              span: 16,
+            }}
+            style={{
+              maxWidth: 600,
+            }}
+            initialValues={{
+              remember: true,
+            }}
+            >
+              <Row gutter={16}>
+              <Col xs={24} sm={12}>
+              <Form.Item
+              label="FullName"
+              name="FullName"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your FullName!',
+                },
+              ]}
+              >
+                <Input />
+                </Form.Item>
+                </Col>
+                <Col xs={24} sm={12}>
+                <Form.Item
+                label="UserName"
+                name="user"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your UserName!',
+                  },
+                ]}
+                ></Form.Item>
+                </Col>
+                </Row>
+                
+                <Row gutter={16}>
+                  {/* Second row with Email and Phone */}
+                  <Col xs={24} sm={12}>
+                  <Form.Item
+                  label="Email Address"
+                  name="email"
+                  rules={[
+                    {
+                      type: 'email',
+                      message: 'Please enter a valid email!',
+                    },
+                    {
+                      required: true,
+                      message: 'Please input your email!',
+                    },
+                  ]}
+                  >
+                    </Form.Item>
+                    </Col>
+                    <Col xs={24} sm={12}>
+                    <Form.Item
+                    label="Phone Number"
+                    name="phone"
+                    rules={[
+                      {
+                        required: true,
+                        message: 'Please input your phone number!',
+                      },
+                    ]}
+                    >
+                      </Form.Item>
+                      </Col>
+                      </Row>
+                      <Form.Item label={null}>
+                        <Button type="primary" htmlType="submit" className='!ml-8' >
+                          Submit
+                        </Button>
+                      </Form.Item>
+                  </Form>
         </div>
     </div>
   )
