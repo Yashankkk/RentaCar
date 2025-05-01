5f8f2f21859fbe255541a58b365aebf180d4efd7
Yashankkk <yashankkalsi030@gmail.com>
2025-05-01 20:35:52 +0530

    initial commit

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-// import Calendar from 'react-calendar'
+import Calendar from '../Components/Calender';
 
 
 const Dashboard = () => {
@@ -13,21 +13,21 @@ const Dashboard = () => {
     
     const navigate = useNavigate();
 
-    // const handleLogout = ()=>{
-    //     localStorage.removeItem('user-info');
-    //     navigate('/login');
-    // }
-
     return (
         <div>
-            <h1 className='text-xl !p-5'>Welcome {user?.username}</h1>
-            {/* <h3>{user?.email}</h3> */}
-            {/* <img src={user?.image} style={{width:200}} alt={user?.name}/> */}
-            {/* <div>
+            <section className="h-[85vh] w-full bg-cover bg-center flex items-center justify-center"
+            style={{ backgroundImage: "url('https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExcTBkdnRlcW9yMDl2Y2Qzc3QxOTR3ZHh1NmxzZ2Vhd3BweGFpNjh2dyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/26tn33aiTi1jkl6H6/giphy.gif')" }}>
+                <h1 className="text-white text-4xl md:text-5xl font-bold bg-black bg-opacity-50 !p-4 rounded-lg">
+                    Cars
+                </h1>
+                <h1 className='text-xl !p-5'>Welcome {user?.username}</h1>
+            <div className='!p-10 '>
                 <Calendar />
-            </div> */}
+            </div>
+            </section>
+
             
-            {/* <button onClick={handleLogout}>Logout</button> */}
+            
         </div>
     )
 }
